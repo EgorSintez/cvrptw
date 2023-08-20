48659758ed1763e26fd638aa856b62fe14c2d228
connectedplanewifi <egor.bulochka@gmail.com>
2023-08-21 00:04:59 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import argparse
 import json
 import math
-import sys
+import pprint
 
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
 
-def cvrptw(**data):
-    demands = data["demands"]
-    depot_idx = data["depot_idx"]
-    time_matrix = data["time_matrix"]
-    time_windows = data["time_windows"]
-    service_time = data["service_time"]
-    capacity = data["capacity"]
-    travel_time = data["travel_time"]
-    timeout = data["timeout"]
+def solve(nodes: int,
+          vehicles: int,
+          depot: int,
+          demands: list,
+          time_matrix: list,
+          time_windows: list,
+          service_time: list,
+          capacity: int,
+          travel_time: int,
+          timeout: int):
 
-    num_nodes = len(demands)
-
-    assert num_nodes == len(time_matrix)
-    assert num_nodes == len(time_windows)
-    assert demands[depot_idx] == 0
+    assert demands[depot] == 0
+    assert nodes == len(time_matrix)
+    assert nodes == len(time_windows)
 
     INF = 1_000_000
 
-    reload_nodes = range(num_nodes, num_nodes + math.ceil(sum(demands) / capacity))
-    num_vehicles = num_nodes
+    reload_nodes = range(nodes, nodes + math.ceil(sum(demands) / capacity))
+    total_nodes = nodes + len(reload_nodes)
 
-    total_nodes = num_nodes + len(reload_nodes)
-
-    manager = pywrapcp.RoutingIndexManager(total_nodes, num_vehicles, depot_idx)
+    manager = pywrapcp.RoutingIndexManager(total_nodes, vehicles, depot)
     routing = pywrapcp.RoutingModel(manager)
 
     def demand_callback(i):
@@ -49,7 +46,7 @@ def cvrptw(**data):
 
     capacity_dimension = routing.GetDimensionOrDie("Capacity")
 
-    for i in range(num_nodes):
+    for i in range(nodes):
         capacity_dimension.SlackVar(i).SetValue(0)
 
     def transit_callback(i, j):
@@ -59,17 +56,17 @@ def cvrptw(**data):
         if from_node in reload_nodes and to_node in reload_nodes:
             return INF
 
-        if from_node in reload_nodes and to_node == depot_idx:
+        if from_node in reload_nodes and to_node == depot:
             return INF
 
-        if from_node == depot_idx and to_node in reload_nodes:
+        if from_node == depot and to_node in reload_nodes:
             return INF
 
         if from_node in reload_nodes:
-            from_node = depot_idx
+            from_node = depot
 
         if to_node in reload_nodes:
-            to_node = depot_idx
+            to_node = depot
 
         return time_matrix[from_node][to_node] + service_time[from_node]
 
@@ -85,27 +82,27 @@ def cvrptw(**data):
                          slack_max=0,
                          capacity=travel_time,
                          fix_start_cumul_to_zero=True,
-                         name="Time")
+                         name="TravelTime")
 
     routing.AddDimension(evaluator_index=cost_evaluator_index,
                          slack_max=0,
                          capacity=INF,
                          fix_start_cumul_to_zero=False,
-                         name="Window")
+                         name="TimeWindow")
 
-    windows_dimension = routing.GetDimensionOrDie("Window")
+    windows_dim = routing.GetDimensionOrDie("TimeWindow")
 
     for i, (a, b) in enumerate(time_windows):
-        if i == depot_idx:
+        if i == depot:
             continue
 
         if i in reload_nodes:
             continue
 
         node = manager.NodeToIndex(i)
-        windows_dimension.CumulVar(node).SetRange(a, b)
+        windows_dim.CumulVar(node).SetRange(a, b)
 
-    for i in range(num_nodes):
+    for i in range(vehicles):
         routing.SetFixedCostOfVehicle(INF, i)
 
     params = pywrapcp.DefaultRoutingSearchParameters()
@@ -118,41 +115,30 @@ def cvrptw(**data):
 
     assert solution
 
-    nodes, vehicles, time = [], [], []
-    vehicle = 0
+    time_dim = routing.GetDimensionOrDie("TimeWindow")
 
-    time_dim = routing.GetDimensionOrDie("Window")
+    nodes_, vehicles_, time_ = [], [], []
 
-    for i in range(len(demands)):
+    for i in range(vehicles):
         index = routing.Start(i)
-        next_index = solution.Value(routing.NextVar(index))
-
-        if routing.IsEnd(next_index):
-            continue
 
         while True:
             time_var = time_dim.CumulVar(index)
             time_to = solution.Max(time_var)
+
+            time_.append(time_to)
+            vehicles_.append(i)
+
             node = manager.IndexToNode(index)
-
-            if node >= len(demands):
-                node = depot_idx
-
-            nodes.append(node)
-            vehicles.append(vehicle)
-            time.append(time_to)
+            node = depot if node in reload_nodes else node
+            nodes_.append(node)
 
             if routing.IsEnd(index):
                 break
 
             index = solution.Value(routing.NextVar(index))
-        vehicle += 1
 
-    return {
-        "nodes": nodes,
-        "vehicles": vehicles,
-        "time": time,
-    }
+    return nodes_, vehicles_, time_
 
 
 if __name__ == "__main__":
@@ -163,5 +149,5 @@ if __name__ == "__main__":
     req = json.load(opt.file)
     opt.file.close()
 
-    res = cvrptw(**req)
-    json.dump(res, sys.stdout, indent=2)
+    nodes, vehicles, time = solve(**req)
+    print(pprint.pformat({"nodes": nodes, "vehicles": vehicles, "time": time}, width=100).replace("'","\""))
